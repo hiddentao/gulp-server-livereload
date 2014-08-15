fe601d729a88cc40cdaf1cdf22fe21841415f1fe
Ramesh Nair <ram@hiddentao.com>
2014-08-15 12:49:43 +0800

    initial

--- src/index.js
+++ src/index.js
@@ -4,25 +4,22 @@ var http = require('http');
 var https = require('https');
 var connect = require('connect');
 var serveStatic = require('serve-static');
-var connectLivereload = require('connect-livereload');
-var tinyLr = require('tiny-lr');
 var watch = require('node-watch');
 var fs = require('fs');
 var serveIndex = require('serve-index');
 var path = require('path');
 var open = require('open');
 var enableMiddlewareShorthand = require('./enableMiddlewareShorthand');
+var socket = require('socket.io');
+
 
 module.exports = function(options) {
-
   var defaults = {
-
     /**
      *
      * BASIC DEFAULTS
      *
      **/
-
     host: 'localhost',
     port: 8000,
     fallback: false,
@@ -45,8 +42,7 @@ module.exports = function(options) {
 
     // Middleware: Livereload
     livereload: {
-      enable:false,
-      port: 35729
+      enable:false
     },
 
     // Middleware: Directory listing
@@ -57,100 +53,88 @@ module.exports = function(options) {
       path: './',
       options: undefined
     }
-
   };
 
   // Deep extend user provided options over the all of the defaults
   // Allow shorthand syntax, using the enable property as a flag
   var config = enableMiddlewareShorthand(defaults, options, ['directoryListing','livereload']);
 
-  var app = connect();
-  
   var openInBrowser = function () {
-  	if(config.open === false) return;
+    if (config.open === false) return;
     open('http' + (config.https ? 's' : '') + '://' + config.host + ':' + config.port);
   };
-  
-  var lrServer;
 
-  if (config.livereload.enable) {
+  // connect app
+  var app = connect()
 
-    app.use(connectLivereload({
-      port: config.livereload.port
-    }));
-
-    lrServer = tinyLr();
-    lrServer.listen(config.livereload.port);
-
-  }
-
+  //  directory listing
   if (config.directoryListing.enable) {
-
       app.use(serveIndex(path.resolve(config.directoryListing.path), config.directoryListing.options));
-
   }
 
-  // Create server
-  var stream = through.obj(function(file, enc, callback) {
-
-    app.use(serveStatic(file.path));
-
-    if (config.fallback) {
-
-      var fallbackFile = file.path + '/' + config.fallback;
-
-      if (fs.existsSync(fallbackFile)) {
-
-        app.use(function(req, res) {
-          fs.createReadStream(fallbackFile).pipe(res);
-        });
-
-      }
-    }
-
-    if (config.livereload.enable) {
-
-      watch(file.path, function(filename) {
-
-        lrServer.changed({
-          body: {
-            files: filename
-          }
-        });
-
-      });
-
-    }
-
-    this.push(file);
-
-    callback();
-
-  });
-
+  // http server
+  var webserver = null;
   if (config.https) {
     var options = {
       key: fs.readFileSync(config.https.key || __dirname + '/../ssl/dev-key.pem'),
       cert: fs.readFileSync(config.https.cert || __dirname + '/../ssl/dev-cert.pem')
     };
-    var webserver = https.createServer(options, app).listen(config.port, config.host, openInBrowser);
+
+    webserver = https.createServer(options, app);
   }
   else {
-    var webserver = http.createServer(app).listen(config.port, config.host, openInBrowser);
+    webserver = http.createServer(app);
   }
 
-  gutil.log('Webserver started at', gutil.colors.cyan('http' + (config.https ? 's' : '') + '://' + config.host + ':' + config.port));
+  // socket.io
+  var io = null;
+  if (config.livereload.enable) {
+    io = require('socket.io').listen(webserver);
 
+    // TODO: inject client JS
+  }
+
+  // Create server
+  var stream = through.obj(function(file, enc, callback) {
+    app.use(serveStatic(file.path));
+
+    if (config.fallback) {
+      var fallbackFile = file.path + '/' + config.fallback;
+
+      if (fs.existsSync(fallbackFile)) {
+        app.use(function(req, res) {
+          fs.createReadStream(fallbackFile).pipe(res);
+        });
+      }
+    }
+
+    if (config.livereload.enable) {
+      watch(file.path, function(filename) {
+        if (io) {
+          io.sockets.emit('reload');
+        }
+      });
+    }
+
+    this.push(file);
+
+    callback();
+  });
+
+
+  // once stream killed
   stream.on('kill', function() {
-
     webserver.close();
 
     if (config.livereload.enable) {
       lrServer.close();
     }
-
   });
 
-  return stream;
+  // start the web server
+  webserver.listen(config.port, config.host, openInBrowser);
 
+  gutil.log('Webserver started at', gutil.colors.cyan('http' + (config.https ? 's' : '') + '://' + config.host + ':' + config.port));
+
+  return stream;
 };
