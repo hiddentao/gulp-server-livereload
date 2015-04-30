dab63323bc53229cd917285be23c1a1f542b6e40
Sean Usick <sean258@gmail.com>
2015-04-30 08:51:51 -0500

    minor fixes

--- bin/livereload.js
+++ bin/livereload.js
@@ -3,22 +3,19 @@
 var server = require('../src/index.js');
 var fs = require('vinyl-fs');
 
-var opts = {
-	port: process.env.PORT,
-	open: true,
-	logLevel: 2
-};
+var opts = {};
 
 var program = require('commander');
 
 program
-  .version('1.2.4')
-  .option('-n, --no-browser', 'No Browser')
-  .option('-l, --log [type]', 'Log level [info]', 'info')
-  .option('-p, --port <n>', 'The port to run on', parseInt)
-  .parse(process.argv);
+	.version('1.2.4')
+	.option('-n, --no-browser', 'Do not open in a Browser')
+	.option('-l, --log [type]', 'Log level [info]', 'info')
+	.option('-p, --port <n>', 'The port to run on', parseInt)
+	.parse(process.argv);
 
-opts.logLevel = program.log;
+if (program.log)
+	opts.log = program.log;
 if (program.noBrowser)
 	opts.open = false;
 if (program.port)
